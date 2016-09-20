8eb58765758181aa9854432e13233beab7811d39
aomine daiki <evillagomesa@yondu.com>
2016-09-20 13:47:19 +0800

    unstable

--- src/main/resources/app/login/login.js
+++ src/main/resources/app/login/login.js
@@ -1,36 +1,31 @@
+//Trigger the alert listener on our backend to bind loginService
 alert("__CONNECT__BACKEND__loginService");
 
+//Create our angular login module
 var loginModule = angular.module('LoginModule',[]);
 
 loginModule.controller('LoginController', function($scope){
     $scope.branches = [];
-
-
     angular.element(document).ready(function () {
         $scope.update();
     });
 
     $scope.update=function(){
+        //This is a java service (magic motherfucker!)
         loginService.loadBranches(function(data){
             $scope.branches = data;
             $scope.$apply();
         });
     }
-
 });
 
-function testArray() {
-    var arr = [];
-    arr.push({id: 1, name: "test"});
-    return arr;
-}
-
+//Functions that are outside the angular context and will be executed by our java backend service
 function loginFailed() {
     $("#login-failed").modal('show');
 
 }
 
 function loginSuccess() {
-    location.href = "../../ocr-view.html";
+    location.href = "../home/home.html";
 }
 

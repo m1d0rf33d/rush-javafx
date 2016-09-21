8ddb5aff8136766aafc4f14d4580fdb673e1cb8d
aomine daiki <evillagomesa@yondu.com>
2016-09-21 18:31:36 +0800

    working state change

--- src/main/resources/app/home.js
+++ src/main/resources/app/home.js
@@ -1,3 +1,6 @@
+//Trigger the alert listener on our backend to bind loginService
+alert("__CONNECT__BACKEND__loginService");
+
 
 var homeModule = angular.module('HomeModule', ['ui.router'])
 .config(function($stateProvider) {
@@ -27,4 +30,23 @@ var homeModule = angular.module('HomeModule', ['ui.router'])
         '</div>'
         })
 
+})
+.controller('HomeController', function($scope, $state){
+    $scope.branches = [];
+    angular.element(document).ready(function () {
+        $scope.update();
+    });
+
+    $scope.update=function(){
+        //This is a java service (magic motherfucker!)
+        loginService.loadBranches(function(data){
+            $scope.branches = data;
+            $scope.$apply();
+        });
+    }
+
+    $scope.clickTest = function() {
+        $state.go('ocr-view');
+        console.log($state);
+    }
 });
